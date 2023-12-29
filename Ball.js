86e0360d3edc1990ea44863e9750646380c23a69
Matías Rodlo <m@hablaqui.com>
2023-12-28 22:17:19 -0300

    documentation

--- Ball.js
+++ Ball.js
@@ -1,12 +1,15 @@
+// Constants
 const INITIAL_VELOCITY = 0.025;
 const VELOCITY_INCREASE = 0.00001;
 
+// Ball class
 export default class Ball {
   constructor(ballElem) {
     this.ballElem = ballElem;
     this.reset();
   }
 
+  // Getters and setters for x and y coordinates
   get x() {
     return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
   }
@@ -23,10 +26,12 @@ export default class Ball {
     this.ballElem.style.setProperty("--y", value);
   }
 
+  // Get rectangle boundaries of the ball
   rect() {
     return this.ballElem.getBoundingClientRect();
   }
 
+  // Reset ball properties to initial state
   reset() {
     this.x = 50;
     this.y = 50;
@@ -41,26 +46,31 @@ export default class Ball {
     this.velocity = INITIAL_VELOCITY;
   }
 
+  // Update ball position and velocity based on time delta and paddle rectangles
   update(delta, paddleRects) {
     this.x += this.direction.x * this.velocity * delta;
     this.y += this.direction.y * this.velocity * delta;
     this.velocity += VELOCITY_INCREASE * delta;
     const rect = this.rect();
 
+    // Check and reverse ball direction on collision with top or bottom edges
     if (rect.bottom >= window.innerHeight || rect.top <= 0) {
       this.direction.y *= -1;
     }
 
+    // Check for collision with paddles and reverse direction
     if (paddleRects.some((r) => isCollision(r, rect))) {
       this.direction.x *= -1;
     }
   }
 }
 
+// Utility function to generate random numbers within a range
 function randomNumberBetween(min, max) {
   return Math.random() * (max - min) + min;
 }
 
+// Utility function to check collision between two rectangles
 function isCollision(rect1, rect2) {
   return (
     rect1.left <= rect2.right &&
--- Paddle.js
+++ Paddle.js
@@ -1,11 +1,14 @@
+// Constants
 const SPEED = 0.02;
 
+// Paddle class
 export default class Paddle {
   constructor(paddleElem) {
     this.paddleElem = paddleElem;
     this.reset();
   }
 
+  // Getter and setter for paddle position
   get position() {
     return parseFloat(
       getComputedStyle(this.paddleElem).getPropertyValue("--position")
@@ -16,14 +19,17 @@ export default class Paddle {
     this.paddleElem.style.setProperty("--position", value);
   }
 
+  // Get rectangle boundaries of the paddle
   rect() {
     return this.paddleElem.getBoundingClientRect();
   }
 
+  // Reset paddle position to initial state
   reset() {
     this.position = 50;
   }
 
+  // Update paddle position based on time delta and ball height
   update(delta, ballHeight) {
     this.position += SPEED * delta * (ballHeight - this.position);
   }
